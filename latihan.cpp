632a663d4f9742073ad1cbd14764a5774a94ec92
Srghhhhhh <ksrghh14@gmail.com>
2026-02-19 14:50:49 +0700

    Add files via upload

---
+++ latihan.cpp
@@ -0,0 +1,14 @@
+//Header
+#include <iostream>
+#include <string>
+using namespace std;
+//main program
+int main(){
+	string nama;
+	int umur;
+	cout<<"Nama : ";
+	getline(cin,nama);
+	cout<<"Umur : ";
+	cin>>umur;
+	
+}
